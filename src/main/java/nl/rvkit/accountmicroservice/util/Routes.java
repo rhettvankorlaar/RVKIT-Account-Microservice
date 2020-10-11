18a8b42676a377fe747516f5228189305e01d9ee
rhettvankorlaar <rhettvankorlaar@gmail.com>
2020-10-11 16:51:39 +0200

    Changes API route

--- src/main/java/nl/rvkit/accountmicroservice/util/Routes.java
+++ src/main/java/nl/rvkit/accountmicroservice/util/Routes.java
@@ -1,7 +1,7 @@
 package nl.rvkit.accountmicroservice.util;
 
 public class Routes {
-    public static final String API_V1 = "/api/v1";
+    public static final String API_V1 = "/v1";
 
     public static class User{
         public static final String USERS = API_V1 + "/users";
